8fd26a8a15642d06d766a12c4d1445e1e06c2437
roynaldi19 <roynaldi19@gmail.com>
2023-03-10 21:38:30 +0700

    terbesar atau maksimal oke

--- app/src/main/java/com/roynaldi19/test/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/test/MainActivity.kt
@@ -8,7 +8,6 @@ import com.roynaldi19.test.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
